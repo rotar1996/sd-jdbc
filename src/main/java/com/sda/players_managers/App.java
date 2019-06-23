78ac72e01da0e8f567ea5f1e74b751aee2f5b761
rotar1996 <rotar.rares@icloud.com>
2019-06-23 10:47:36 +0300

    initial commit

---
+++ src/main/java/com/sda/players_managers/App.java
@@ -0,0 +1,13 @@
+package com.sda.players_managers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
